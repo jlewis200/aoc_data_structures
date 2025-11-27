4892364a111df6f62711a081712689b88f204bd1
Justin <18606523+jlewis200@users.noreply.github.com>
2025-11-27 13:43:29 -0500

    Add tests for manhattan() method.

--- aoc_data_structures/data_structures/tests/test_vector_tuple.py
+++ aoc_data_structures/data_structures/tests/test_vector_tuple.py
@@ -19,3 +19,39 @@ class TestVectorTuple(unittest.TestCase):
             VectorTuple(1, 1) + VectorTuple(1, 2),
             VectorTuple(2, 3),
         )
+
+    def test_manhattan_0(self):
+        """
+        Test manhattan() method with magnitude 0.
+        """
+        self.assertEqual(
+            VectorTuple(0, 0).manhattan(),
+            0,
+        )
+
+    def test_manhattan_negative(self):
+        """
+        Test manhattan() method with negative values.
+        """
+        self.assertEqual(
+            VectorTuple(-2, -2).manhattan(),
+            4,
+        )
+
+    def test_manhattan_positive(self):
+        """
+        Test manhattan() method with positive values.
+        """
+        self.assertEqual(
+            VectorTuple(2, 2).manhattan(),
+            4,
+        )
+
+    def test_manhattan_mixed_sign(self):
+        """
+        Test manhattan() method with positive and negative values.
+        """
+        self.assertEqual(
+            VectorTuple(2, -2).manhattan(),
+            4,
+        )
